65351e1ecf9a3555f7e1c358e298e20d35601653
Nikita Patil <patilnikita296@gmail.com>
2024-01-14 23:57:53 +0100

    Added Execute button

--- demo1.py
+++ demo1.py
@@ -17,6 +17,9 @@ def convert_to_decimal():
         entry_decimal1.insert(0, str(decimal1))
         entry_decimal2.insert(0, str(decimal2))
         entry_decimal3.insert(0, str(decimal3))
+
+        result_label.config(text="")
+
     except ValueError:
         result_label.config(text="Invalid hexadecimal input. Please enter valid hex numbers.")
 
@@ -25,18 +28,18 @@ root = tk.Tk()
 root.title("DET Module Helper")
 
 # Create labels and entry widgets for hex inputs
-label_hex1 = tk.Label(root, text="Module Id:")
-label_hex2 = tk.Label(root, text="API Id:")
-label_hex3 = tk.Label(root, text="Error Id:")
+label_hex1 = tk.Label(root, text="Module Id")
+label_hex2 = tk.Label(root, text="API Id")
+label_hex3 = tk.Label(root, text="Error Id")
 
 entry_hex1 = tk.Entry(root)
 entry_hex2 = tk.Entry(root)
 entry_hex3 = tk.Entry(root)
 
 # Create labels and entry widgets for decimal outputs
-label_decimal1 = tk.Label(root, text="Module Name:")
-label_decimal2 = tk.Label(root, text="API :")
-label_decimal3 = tk.Label(root, text="Error Description:")
+label_decimal1 = tk.Label(root, text="Module Name")
+label_decimal2 = tk.Label(root, text="API")
+label_decimal3 = tk.Label(root, text="Error Description")
 
 entry_decimal1 = tk.Entry(root, state=tk.DISABLED)  # Disable editing
 entry_decimal2 = tk.Entry(root, state=tk.DISABLED)
@@ -46,7 +49,7 @@ entry_decimal3 = tk.Entry(root, state=tk.DISABLED)
 result_label = tk.Label(root, text="", fg="red")
 
 # Create the execute button
-execute_button = tk.Button(root, text="Execute", command=convert_to_decimal)
+execute_button = tk.Button(root, text="Execute", command=convert_to_decimal, width=50)
 
 # Organize widgets in the grid layout
 label_hex1.grid(row=0, column=0, padx=5, pady=5)
@@ -66,7 +69,7 @@ entry_decimal3.grid(row=2, column=3, padx=5, pady=5)
 
 result_label.grid(row=3, column=0, columnspan=4, padx=5, pady=5)
 
-execute_button.grid(row=4, column=0, columnspan=4, pady=10)
+execute_button.grid(row=4, column=0, columnspan=4, pady=10 , padx=30)
 
 # Start the Tkinter event loop
 root.mainloop()
